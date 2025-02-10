91598a199509c988db494b58e6ac5112f54ab5ba
Shaikmaaz99 <SHAIK.20211ECE0005@presidencyuniversity.in>
2025-02-10 15:20:48 +0530

    file is added

---
+++ Student.java
@@ -0,0 +1,15 @@
+//class Student{
+//public static void main(String args[]){
+//int age=23;
+//System.out.println("The age of the Student is :"+age);
+//}
+//}
+import java.util.*;
+class StudentAge{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the age of the student:");
+int age=sc.nextInt();
+System.out.println("The age of the student"+age);
+}
+}
